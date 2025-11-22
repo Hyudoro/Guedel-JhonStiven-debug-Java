17bb9469540335b4bbfd5621c477f6e1533ad32c
Jhon-Stiven <jh.virtual.tech@gmail.com>
2025-11-22 12:31:46 +0100

    methods implementations for modularity into the main separating reading, counting, sorting

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,6 +7,8 @@ import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class AnalyticsCounter {
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
 	/**
 	 * Main entry point for counting and sorting symptoms.
 	 *
@@ -18,25 +20,59 @@ public class AnalyticsCounter {
 	 * 5. Writes the result to an output file.
 	 * 6. Prints the result to the console.
 	 */
+
+	public AnalyticsCounter(ISymptomReader iSymptomReader, ISymptomWriter iSymptomWriter){
+		this.reader = iSymptomReader;
+		this.writer = iSymptomWriter;
+	}
+
+
+
 	public static void main(String[] args) {
 		ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt");
-		List<String> symptoms = reader.GetSymptoms();
-		Map<String, Long> symptomCounts = symptoms.stream()
+		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+		AnalyticsCounter counter = new AnalyticsCounter(reader, writer);
+
+		List<String> symptoms = counter.getSymptoms();
+		Map<String, Long> symptomsByOccurences = counter.countBySymptoms(symptoms);
+        Map<String, Long> symtomsRankedAlphabetically = counter.SortSymptomsAlphabetically(symptomsByOccurences);
+		
+
+
+
+//		/try {
+//			writer.writeSymptoms(sortedSymptoms);
+//		} catch (IOException e) {
+//			e.printStackTrace(); // handling errors temporarely
+//		}
+//		sortedSymptoms.forEach((k, v) -> System.out.println(k + ": " + v));
+//	}
+	}
+
+	public List<String> getSymptoms(){
+		return reader.getSymptoms();
+	}
+
+
+
+
+	public static Map<String, Long> countBySymptoms(List<String> symptoms){
+		Map<String, Long> count = symptoms
+				.stream()
 				.map(String::trim)
 				.filter(s -> !s.isEmpty())
 				.collect(Collectors.groupingBy(
-						s -> s,
+						s->s,
 						Collectors.counting()
 				));
-		Map<String, Long> sortedSymptoms = new TreeMap<>(symptomCounts);
-		ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
-
-		try {
-			writer.writeSymptoms(sortedSymptoms);
-		} catch (IOException e) {
-			e.printStackTrace(); // handling errors temporarely
-		}
-		sortedSymptoms.forEach((k, v) -> System.out.println(k + ": " + v));
+	    return count;
 	}
-}
+
+	public Map<String,Long> SortSymptomsAlphabetically(Map<String, Long> naturalRanking){
+		return new TreeMap<>(naturalRanking);
+	}
+
+
+ }
+
 
